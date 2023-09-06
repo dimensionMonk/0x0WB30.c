06a7420b01ccfe6eef5e1ac3d739d2a0a913ddc0
dimensionMonk <142955643+dimensionMonk@users.noreply.github.com>
2023-09-06 14:14:08 -0700

    Create no_l.cpp
    delete ~

---
+++ no_l.cpp
@@ -0,0 +1,26 @@
+~LEAK : 123 "SYSTEM" for(FUNCTION;231<<0);SYSTEM)//HANNDLE ERROR
+~LEAK_FORWORD "1" + SYSTEM '2' = DIMENSION(3) {FLIP}-{SPIN}
+'SYSTEM' + 'BTU' {float} - {FLIP} [0.988650] {SPICES} IF~
+"SYSTEM_POWER" + "SYSTEM_CODE" = "SYSTEM_CAPTION" {DATA}-{FLIP}='CODE'
+"SYSTEM" + "SLEEP" --IMAGE {SYSTEM} + {FLIP} = ARRAY[DIMENSION]
+"SPRED" '8' = '24' + '24' + '24' = '60' float(2.4) = ARRAY
+{SPIN} - {LOCK} -{
+}UNLOCK - {SPIN} - {FLIP} = LOCK{
+UNLOCK}-{FLIP} -> SLEEP--IMAGE --PROSCESSIE 'FLIP' (2)
+{UNLOCK} - (3) : 010 {SUPERPISION} + 'PROCESSIE' : 10
+{LOCK}-{SUPERPISION} -[float]*PRT (2)
+------------------------------------------------------------------
+tools->center->tool(clean : tool)
+tool->center->tool(return : files)
+tool-debugger->file(system : INT)
+debugger-tool->spin(stat : INT)
+debugger-tool-matrix->system-->file : flip
+debugger-tool-system->system int
+file-> int();
+int system .. char MARTIX :: debugger return 'function' + '/0'
+for(stat:MARTIX;0<<648;CONC++) true(0) : system : stat[0N]
+spin_martix_debugger(bit-86) + false(1)[d+ctr] + [lsb] true() ;
+if(debugger:MATRIX;0<<64;bit++) while(binary == loadmodule)
+function"sleep"./kick = system true()
+//system debug
+//author : MICHAEL s.b
