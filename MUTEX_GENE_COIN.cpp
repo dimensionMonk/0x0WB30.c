b3dddb1f79466e0afbbb38220c5cc643a0853a93
dimensionMonk <142955643+dimensionMonk@users.noreply.github.com>
2023-09-06 04:10:29 -0700

    Create MUTEX_GENE_COIN.cpp
    make : files -> mutex "morals"

---
+++ MUTEX_GENE_COIN.cpp
@@ -0,0 +1,6 @@
+static (hide) : people : count(binary;0<<843;list++)
+people : static : count(avar;0<<846716;gene++) : system
+{
+for(gene;16<<0;avr) + '0x0' :: {species}-{conc}-system [0ff]
+}hal_led :: spices :-: TX [read] + [right] : [exe]
+|| gene || terminal:NUL:static : MUTEX_file_RETURN() // author : michael b.s
