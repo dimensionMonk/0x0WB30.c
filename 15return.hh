2a3678d28fa2f132fa1d4d4b31b43172babf7484
dimensionMonk <142955643+dimensionMonk@users.noreply.github.com>
2023-09-06 02:33:57 -0700

    Create 15return.hh
    system -proscess true debugge

---
+++ 15return.hh
@@ -0,0 +1,14 @@
+tools->center->tool(clean : tool)
+tool->center->tool(return : files)
+tool-debugger->file(system : INT)
+debugger-tool->spin(stat : INT)
+debugger-tool-matrix->system-->file : flip
+debugger-tool-system->system int
+file-> int();
+int system .. char MARTIX :: debugger return 'function' + '/0'
+for(stat:MARTIX;0<<648;CONC++) true(0) : system : stat[0N]
+spin_martix_debugger(bit-86) + false(1)[d+ctr] + [lsb] true() ;
+if(debugger:MATRIX;0<<64;bit++) while(binary == loadmodule)
+function"sleep"./kick = system true()
+//system debug
+//author : MICHAEL s.b
