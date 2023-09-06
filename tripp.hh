a5dc3e82edb7ebf1dcf316f6e92ced4dc80002dd
dimensionMonk <142955643+dimensionMonk@users.noreply.github.com>
2023-09-06 03:24:12 -0700

    Create tripp.hh
    tv extenction //software || geneware : transhuminod

---
+++ tripp.hh
@@ -0,0 +1,7 @@
+int key >> text >> stx //EOT : SOT 
+char text >> tx >> return\\ SOT - SYX
+tv << screen >> tx << transformer [xyyx] + [yxxy] = ZxY1-yx-Zy ;
+{
+for(tv;0<<9830;stack++)
+}AVR : AGM {image} data_resort_spreed -delete "not" {spread} system{true}0
+//authour : michael s.b
