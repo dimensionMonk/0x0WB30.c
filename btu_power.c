e3e676908da5cc1f82b31ae28b51609909fba1c4
dimensionMonk <142955643+dimensionMonk@users.noreply.github.com>
2023-09-06 02:45:39 -0700

    Create btu_power.c
    power- 1 kHz - hour : btu return

---
+++ btu_power.c
@@ -0,0 +1,12 @@
+~LEAK : 123 "SYSTEM" for(FUNCTION;231<<0);SYSTEM)//HANNDLE ERROR
+~LEAK_FORWORD "1" + SYSTEM '2' = DIMENSION(3) {FLIP}-{SPIN}
+'SYSTEM' + 'BTU' {float} - {FLIP} [0.988650] {SPICES} IF~
+"SYSTEM_POWER" + "SYSTEM_CODE" = "SYSTEM_CAPTION" {DATA}-{FLIP}='CODE'
+"SYSTEM" + "SLEEP" --IMAGE {SYSTEM} + {FLIP} = ARRAY[DIMENSION]
+"SPRED" '8' = '24' + '24' + '24' = '60' float(2.4) = ARRAY
+{SPIN} - {LOCK} -{
+}UNLOCK - {SPIN} - {FLIP} = LOCK{
+UNLOCK}-{FLIP} -> SLEEP--IMAGE --PROSCESSIE 'FLIP' (2)
+{UNLOCK} - (3) : 010 {SUPERPISION} + 'PROCESSIE' : 10
+{LOCK}-{SUPERPISION} -[float]*PRT (2) //aouthor : michael b.s
+------------------------------------------------------------------
